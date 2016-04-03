bc1a7f852fe94eb718af3d3fbf6e6c134555e6c8
张瑞 <rui.crater@gmail.com>
2016-04-03 13:09:36 +0800

    新增运行选项功能，可单独运行h2数据库

--- src/main/java/com/umbrellary/BeatlesApplication.java
+++ src/main/java/com/umbrellary/BeatlesApplication.java
@@ -1,7 +1,10 @@
 package com.umbrellary;
 
+import com.umbrellary.beatles.CSDNBlogPageProcessor;
 import com.umbrellary.beatles.CSDNNewsPageProcessor;
 import com.umbrellary.beatles.DatabasePipeline;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -16,23 +19,42 @@ import us.codecraft.webmagic.Spider;
 @ComponentScan
 public class BeatlesApplication implements CommandLineRunner {
 
+    private static final Logger logger = LoggerFactory.getLogger(BeatlesApplication.class);
+
+    private static String[] args;
+
+    public static String[] getArgs() {
+        return args;
+    }
+
+    public static void setArgs(String[] args) {
+        BeatlesApplication.args = args;
+    }
+
     public static void main(String[] args) {
+        setArgs(args);
         SpringApplication.run(BeatlesApplication.class, args);
     }
 
     @Override
     public void run(String... strings) throws Exception {
-//        Spider.create(new CSDNBlogPageProcessor())//
-//                .addUrl("http://blog.csdn.net/?&page=2")//
-//                .addPipeline(new DatabasePipeline())//
-//                .thread(5)//
-//                .run();
 
-        Spider.create(new CSDNNewsPageProcessor())//
-                .addUrl("http://news.csdn.net/news/2")//
-                .addPipeline(databasePipeline)//
-                .thread(5)//
-                .run();
+        if (getArgs()[0].equals("web1")) {
+            Spider.create(new CSDNNewsPageProcessor())
+                    .addUrl("http://news.csdn.net/news/2")
+                    .addPipeline(databasePipeline)
+                    .thread(5)
+                    .run();
+        } else if (getArgs()[0].equals("web2")) {
+            Spider.create(new CSDNBlogPageProcessor())
+                    .addUrl("http://blog.csdn.net/?&page=2")
+                    .addPipeline(new DatabasePipeline())
+                    .thread(5)
+                    .run();
+
+        } else if (getArgs()[0].equals("h2server")) {
+            logger.info("H2 started success!");
+        }
     }
 
     @Autowired
